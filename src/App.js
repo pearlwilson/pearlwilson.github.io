1386b51a5e79f0c641058ddb270bdd62677c6b9e
Pearl Wilson <46859842+pearlwilson@users.noreply.github.com>
2023-09-13 18:33:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hi!!!!!!!!!!!!!!!!!!!!</h1>
+      <h1 className="header">Hi!</h1>
+      <p className="subtext">Welcome to our awesome website.</p>
     </div>
   );
 }
